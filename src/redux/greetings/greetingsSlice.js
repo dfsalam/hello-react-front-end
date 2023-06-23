688b4d529c34bf7dff2bcf321f87c40628f1e23a
dfsalam <dfsalam@gmail.com>
2023-06-22 22:00:17 -0500

    Add greetingSlice and store.

---
+++ src/redux/greetings/greetingsSlice.js
@@ -0,0 +1,42 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const initialState = {
+  greetings: [],  
+  isLoading: false,
+  errors: null,
+};
+const URL = 'http://localhost:3000/api/v1/greetings';
+export const getGreetings = createAsyncThunk('greetings/getGreetings',
+  async () => {   
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return error.message();
+    }
+  });
+
+const greetingsSlice = createSlice({
+  name: 'greetings',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder     
+      .addCase(getGreetings.pending, (state) => ({
+        ...state,
+        isLoading: true,
+      }))
+      .addCase(getGreetings.fulfilled, (state, action) => ({
+        ...state,
+        isLoading: false,
+        greetings: action.payload,
+      }))
+      .addCase(getGreetings.rejected, (state) => ({
+        ...state,
+        isLoading: false,
+      }));
+  },
+});
+
+export default greetingsSlice.reducer;
